b82188f33bc11251dcdfe7585a2bf86d2dcb4f32
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-06 21:55:49 -0500

    add LocalFiles folder

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev
-sudo apt install ruby-dev ruby-bundler nodejs
+#sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev
+#sudo apt install ruby-dev ruby-bundler nodejs
 gem install nokogiri
 #bundle clean
 bundle install
