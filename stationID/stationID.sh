83bab6fb8cde3e058180d8a9fa582b08da26d00d
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-08 20:32:18 -0400

    more stationID.sh

--- stationID/stationID.sh
+++ stationID/stationID.sh
@@ -7,17 +7,55 @@
 rm -f *.aiff
 rm -f *.mp3
 
-for (( i=0; i<10; ++i)); do
+COUNT=1
+
+for (( i=1; i<i=1000; ++i)); do
+
+for letter in {A..Z} ; do
+  echo $letter
+
+for vowel in {a,e,i,o,u} ; do
+  echo $vowel
+
 
 DATE=$(date +%s)
-say -v fred "You are listening to B T C Radio Dot Net" -o stationID_$DATE.aiff
+say -v fred "You are listening to B T C Radio Dot Net" -o stationID.$DATE.aiff
 
-lame  stationID_$DATE.aiff  stationID_$DATE.mp3
+lame  stationID.$DATE.aiff $letter$vowel.$DATE.mp3
+lame  stationID.$DATE.aiff  $COUNT.$DATE.mp3
 
+
+
+
+
+id3v2 -t $COUNT."btcRadio.net" $COUNT.$DATE.mp3
+sleep 0.5
+id3v2 -a $COUNT."btcRadio.net" $COUNT.$DATE.mp3
+sleep 0.5
+id3v2 -A $COUNT."btcRadio.net" $COUNT.$DATE.mp3
+sleep 0.5
+id3v2 -c $COUNT."btcRadio.net":"StationID":"en" $COUNT.$DATE.mp3
+sleep 0.5
+id3v2 --WORS "http://listen.btcRadio.net" $COUNT.$DATE.mp3
+sleep 0.5
+#
+id3v2 -t $letter$vowel."btcRadio.net" $letter$vowel.$DATE.mp3
+sleep 0.5
+id3v2 -a $letter$vowel."btcRadio.net" $letter$vowel.$DATE.mp3
+sleep 0.5
+id3v2 -A $letter$vowel."btcRadio.net" $letter$vowel.$DATE.mp3
+sleep 0.5
+id3v2 -c $letter$vowel."btcRadio.net":"StationID":"en" $letter$vowel.$DATE.mp3
+sleep 0.5
+id3v2 --WORS "http://listen.btcRadio.net" $letter$vowel.$DATE.mp3
+sleep 0.5
+#
 rm -f *.aiff
+sleep 0.5
 
-sleep 2
+((COUNT+=10))
 
 done
+done
+done
 
-#TODO  add idv3v2 commands
