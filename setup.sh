ad6ff0a0082967b56505b4425e16cd00a6f396d1
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-10 16:13:08 -0400

    add smd

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ setup() {
 
         #install brew libs
 	brew cask install spotify
+	brew cask install dbus pkg-config portaudio
+	brew cask install LaunchControl
         brew install id3v2
 	brew install lame
         brew install node
@@ -18,7 +20,7 @@ setup() {
 
         brew install coreutils #gshuf
         #  PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-        brew cask install docker
+        #brew cask install docker
         #brew link --overwrite docker
         #ls -l /usr/local/bin/docker*
         #open /Applications/Docker.app
@@ -33,13 +35,28 @@ setup() {
 }
 setup
 
+git clone https://github.com/btcradio/smd.git
+cd smd
+pip3 install -r requirements.txt
+pip3 install PyQtWebEngine
+pip3 install pywebview[qt]
+cd ..
+
+
+git clone https://github.com/btcradio/spotify-playlist-generator.git
+
+git clone https://github.com/btcradio/spotifyd.git
+
+git clone git://anongit.freedesktop.org/pulseaudio/pulseaudio
+
+
 #sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev
 #sudo apt install ruby-dev ruby-bundler nodejs
-gem install bundler
-gem install nokogiri
+#gem install bundler
+#gem install nokogiri
 
 #gem install commonmarker -v '0.17.13' --source 'https://rubygems.org/'
-bundle clean --force
-bundle install
+#bundle clean --force
+#bundle install
 
-bundle exec jekyll serve
+#bundle exec jekyll serve
